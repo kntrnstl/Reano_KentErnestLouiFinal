a5b45004cef28405628e4a5e3a65faea372631a1
mjramirez <michaeljustinramirez5@gmail.com>
2025-09-10 23:36:12 +0800

    First commit

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'crudui',
+    'username'	=> 'sql12798093',
+    'password'	=> 'ArLI5eE8Dm',
+    'database'	=> 'sql12798093',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
